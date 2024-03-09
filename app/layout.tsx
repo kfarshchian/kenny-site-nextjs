5ab97f63aed9990f47793ddb30bb3c3938d8849e
Kameron  Farshchian <kfarshchian@gmail.com>
2024-03-08 21:43:19 -0700

    speed test

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
 import ThisScript from './ThisScript/ThisScript'
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ['latin'] })
 import {GoogleAnalyticsTracking} from '../components/GoogleAnalyticsTracking/GoogleAnalyticsTracking'
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <meta name="robots" content="all" />
       <GoogleAnalyticsTracking/>
       <body>
+      <SpeedInsights/>
       <Navbar />
       <main>
       <ThisScript />
