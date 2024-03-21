e76853049117a4a691bd8a216277570616a5aacb
Kameron  Farshchian <kfarshchian@gmail.com>
2024-03-21 02:02:32 -0600

    test graph twitter

--- app/page.jsx
+++ app/page.jsx
@@ -28,6 +28,8 @@ import reverseMortgage from "../public/reverse-mortgage-utah-retirement3.png";
 export const metadata = {
   title: "Utah Mortgage Expert - Kenny Farshchian | PRMI",
   description: "Connect with Kenny Farshchian, Utah's premier mortgage and reverse mortgage expert at Primary Residential Mortgage, Inc. for all your home financing needs.",
+  url: 'www.mortgagekenny.com',
+  type: 'website',
   openGraph: {
     url: 'www.mortgagekenny.com',
     title: 'Utah Mortgage Expert - Kenny Farshchian | PRMI',
@@ -36,7 +38,7 @@ export const metadata = {
     twitter: {
       card: "summary_large_image",
       title: "Utah Mortgage Expert - Kenny Farshchian | PRMI",
-      description: "Connect with Kenny Farshchian, Utah's premier mortgage and reverse mortgage expert at Primary Residential Mortgage, Inc. for all your home financing needs",
+      description: "Connect with Utah's premier mortgage expert Kenny Farshchian at Primary Residential Mortgage, Inc. for all your home financing needs.",
       images: ["https://www.mortgagekenny.com/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fsmalllogokenny.8cfc83f9.png&w=1080&q=75"],
     },
   }
